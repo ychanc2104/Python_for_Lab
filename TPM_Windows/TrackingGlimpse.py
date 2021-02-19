8a655803875831ed6bf4b5769f4c2f02f03df6da
pine2104 <ychanc2104@gmail.com>
2021-02-19 16:36:13 +0800

    #16 Add comments for tracking flowchart

--- TPM_Windows/TrackingGlimpse.py
+++ TPM_Windows/TrackingGlimpse.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 Flowchart
-1. Localization
-2. Tracking with 2D Gaussian
-3. Save four files for fitting results
+1. Localization:
+(a) get average image of N_loc pictures
+(b) get contours using Canny edge detection algorithm
+(c) get edges of contours, and use image moment of edges to get center of positions, (x,y)
+(d) get avg. intensity of each aoi, and remove aoi which avg. intensity < blacklevel
+(e) sort (x,y) of each aoi according to distance between y-axis(x=0)
+(f) select one aoi of each cluster. cluster: all aoi which distance < criteria_dist
+(g) fit each aoi with 2D Gaussian to get accurate (x,y)
+(h) draw aoi circle and show(save or not) figure to 'output.png'
+
+2. Tracking all aoi with 2D Gaussian
+
+3. Save fitting cideo (optional)
+
+4. Save four files for fitting results
+
 """
 
 ### import used modules first
