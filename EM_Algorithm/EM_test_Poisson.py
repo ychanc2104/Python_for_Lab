1776f7b6f1f8e417b08c48608d9d96543e7a6e96
pine2104 <ychanc2104@gmail.com>
2021-03-12 16:00:29 +0800

    #10(EM) EM fit for multiple Poisson R.V.

--- EM_Algorithm/EM_test_Poisson.py
+++ EM_Algorithm/EM_test_Poisson.py
@@ -4,8 +4,46 @@ from basic.binning import binning
 import numpy as np
 import matplotlib.pyplot as plt
 
-def exp_pdf(t, tau):
-    y = 1/tau*np.exp(-t/tau)
+def exp_EM(data, n_components, tolerance=10e-5):
+    f, tau = init(data, n_components=n_components)
+    improvement = 10
+    j = 0
+    f = np.ones(n_components) / n_components
+    tau_save = []
+    f_save = []
+    while (j < 10 or improvement > tolerance) and j < 5000:
+        tau_save, f_save = collect_EM_results([tau_save, tau], [f_save, f])
+        likelihood = weighting(data, tau, f)
+        tau, f = update_tau_f(likelihood, data)
+        improvement = cal_improvement([tau_save[-n_components:], tau], [f_save[-n_components:], f])
+        j += 1
+    f_save = np.reshape(f_save, (j, n_components))
+    tau_save = np.reshape(tau_save, (j, n_components))
+    return f_save, tau_save
+
+##  initialize parameters
+def init(data, n_components):
+        mean = np.mean(data)
+        std = np.std(data, ddof=1)
+        f = np.ones(n_components) / n_components
+        tau = np.arange(abs(mean - 0.5 * std), mean + 0.5 * std + 1, 1*std/(n_components - 1))
+        return f, tau
+
+##  args: list
+def exp_pdf(t, args):
+    f = np.array(args[0])
+    tau = np.array(args[1])
+    try:
+        n_col = len(t)
+    except:
+        n_col = 1
+    try:
+        n_row = len(f)
+    except:
+        n_row = 1
+    y = np.zeros((n_row, n_col))
+    for i in range(n_row):
+        y[i,:] = f[i] * 1/tau[i]*np.exp(-t/tau[i])
     return y
 
 def ln_exp_pdf(t, tau):
@@ -19,8 +57,8 @@ def weighting(data, tau, f):
     n_sample = len(data)
     likelihood = np.zeros((n_components, n_sample))
     for i, x in enumerate(data):
-        p = ln_exp_pdf(x, tau)
-        likelihood[:, i] = f * np.exp(p) / sum(f * np.exp(p))
+        p = exp_pdf(x, [f,tau])
+        likelihood[:, i] = (p / sum(p)).ravel()
     return likelihood
 
 ##  update mean, std and fraction using matrix multiplication, (n_feture, n_sample) * (n_sample, 1) = (n_feture, 1)
@@ -30,64 +68,49 @@ def update_tau_f(likelihood, data):
     f = np.sum(likelihood, axis=1) / n_sample
     return tau, f
 
-def plot_EM_results(tau, f):
-    fig, axs = plt.subplots(2, sharex=True)
-    axs[1].set_xlabel('iteration', fontsize=15)
-    plot_EM_result(tau, axs[0], ylabel='mean')
-    plot_EM_result(f, axs[1], ylabel='fraction')
+def plot_EM_results(results, labels):
+    n_components = len(results)
+    fig, axs = plt.subplots(n_components, sharex=True)
+    axs[-1].set_xlabel('iteration', fontsize=15)
+    for i in range(n_components):
+        plot_EM_result(results[i], axs[i], ylabel=f'{labels[i]}')
 
+##  result: (iteration, n_components)
 def plot_EM_result(result, ax, xlabel='iteration', ylabel='value'):
-    n_feature = 1
     iteration = result.shape[0]
-    for i in range(n_feature):
+    n_components = result.shape[1]
+    for i in range(n_components):
         ax.plot(np.arange(0, iteration), result[:, i], '-o')
     ax.set_ylabel(f'{ylabel}', fontsize=15)
 
-if __name__ == "__main__":
-    n_sample = 500
-    data = gen_poisson(tau=[0.1, 2, 10], n_sample=[200, 200, 200])
-    n_sample = len(data)
-    # likelihood = weighting(data, tau, f)
-    # tau2, f2 = update_tau_f(likelihood, data)
-    tau = [0.4, 10, 15]
-    # tau= [5]
-    n_components = len(tau)
-    improvement = 10
-    tolerance = 10e-5
-    j = 0
-    f = np.ones(n_components) / n_components
-    tau_save = []
-    f_save = []
-    while (j < 10 or improvement > tolerance) and j < 5000:
-        tau_save, f_save = collect_EM_results([tau_save, tau], [f_save, f])
-        likelihood = weighting(data, tau, f)
-        tau, f = update_tau_f(likelihood, data)
-        improvement = cal_improvement([tau_save[-n_components:], tau], [f_save[-n_components:], f])
-        j += 1
-    tau_save = np.reshape(tau_save, (j ,n_components))
-    f_save = np.reshape(f_save, (j ,n_components))
-    ##  plot EM results(mean, std, fractio with iteration)
-    plt.plot(np.arange(0, j), tau_save, 'o-')
-    plt.figure()
-    plt.plot(np.arange(0, j), f_save, 'o-')
-
-    label = []
-    for i, x in enumerate(data):
-        p = likelihood[:, i]
-        label += [np.argmax(p)]
-    label = np.array(label)
-    data_cluster = [data[label == i] for i in range(n_components)]
-
+##  plot data histogram and its gaussian EM (GMM) results, results:
+def plot_fit_pdf(data, function, results):
+    n_components = len(results[0])
     bin_width = (12/n_sample)**(1/3)*np.mean(data)/n_components**1.3 ## scott's formula for poisson process
     bin_number = int((max(data)-min(data))/bin_width)
-    pd, center = binning(data, bin_number, xlabel='dwell time (s)')
-    
+    pd, center = binning(data, bin_number) # plot histogram
+    data_std_new = np.std(data, ddof=1)
     x = np.arange(0.1, max(data), 0.01)
-    y_fit = np.array([f[i]*exp_pdf(x, tau[i]) for i in range(n_components)])
+    y_fit = function(x, results)
     for i in range(n_components):
         plt.plot(x, y_fit[i,:], '-')
     plt.plot(x, sum(y_fit), '-')
     plt.xlabel('dwell time (s)', fontsize=15)
     plt.ylabel('probability density (1/$\mathregular{s^2}$)', fontsize=15)
-    plt.xlim([0,np.mean(data)*2])
-    
+    plt.xlim([0, data_std_new*2])
+
+
+if __name__ == "__main__":
+    ##  produce data
+    data = gen_poisson(tau=[0.1, 2, 10], n_sample=[200, 200, 200])
+    n_sample = len(data)
+
+    ##  fit EM
+    n_components = 3
+    f_save, tau_save = exp_EM(data, n_components, tolerance=10e-5)
+
+    ##  plot EM results(mean, std, fractio with iteration)
+    plot_EM_results([f_save, tau_save], ['fraction', 'dwell time'])
+
+    ##  plot data histogram and its gaussian EM (GMM) results
+    plot_fit_pdf(data, exp_pdf, [f_save[-1,:], tau_save[-1,:]])
