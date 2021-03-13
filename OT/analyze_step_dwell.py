04f17bdd0e5695fd280d1621b784a29af4ab0694
pine2104 <ychanc2104@gmail.com>
2021-03-13 16:02:06 +0800

    #2(OT) Refine code

--- OT/analyze_step_dwell.py
+++ OT/analyze_step_dwell.py
@@ -8,13 +8,35 @@ from basic.select import get_files, select_folder
 import scipy.io as sio
 import numpy as np
 
-conc = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0]
+def GMM_results(data, n_components, tolerance=10e-5):
+    ### fit GMM
+    m_save, s_save, f_save, label, data_cluster = GMM(data, n_components, tolerance=tolerance)
+    ##  plot EM results(mean, std, fractio with iteration)
+    plot_EM_results(m_save, s_save, f_save)
+    ##  plot data histogram and its gaussian EM (GMM) results
+    plot_fit_gauss(step, f_save[-1,:], m_save[-1,:], s_save[-1,:])
+
+    return f_save[-1,:], m_save[-1,:] ,s_save[-1,:]
+
+def poiEM_results(data, n_components, tolerance=10e-5):
+    ##  fit EM
+    f_save, tau_save = exp_EM(data, n_components, tolerance=tolerance)
+    ##  plot EM results(mean, std, fractio with iteration)
+    plot_EM_results_exp([f_save, tau_save], ['fraction', 'dwell time'])
+    ##  plot data histogram and its gaussian EM (GMM) results
+    plot_fit_pdf(dwell, exp_pdf, [f_save[-1,:], tau_save[-1,:]])
+
+    return f_save[-1,:], tau_save[-1,:]
+
+
+# conc = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0]
+conc = [0.0, 0.5]
 
 path_folder = select_folder()
 all_step = []
 all_dwell = []
 n_components_g = 2
-n_components_e = 2
+n_components_p = 1
 
 centers = []
 stds = []
@@ -30,34 +52,20 @@ for c in conc:
         data = sio.loadmat(path)
         step = np.append(step, data['step'])
         dwell = np.append(dwell, [data['dwell']])
-    m_save, s_save, f_save, label, data_cluster = GMM(step, n_components_g, tolerance=10e-5)
-    m = m_save[-1,:]
-    s = s_save[-1,:]
-    f = f_save[-1,:]
-    ##  plot EM results(mean, std, fractio with iteration)
-    plot_EM_results(m_save, s_save, f_save)
 
-
-    ### fit dwell
-    ##  plot data histogram and its gaussian EM (GMM) results
-    plot_fit_gauss(step, f, m, s)
+    all_step += [step]
+    all_dwell += [dwell]
+    ### get GMM results
+    f, m, s = GMM_results(step, n_components_g, tolerance=10e-5)
     centers += [m]
     stds += [s]
     fractions += [f]
 
-    ##  fit EM
-    f_save, tau_save = exp_EM(dwell, n_components_e, tolerance=10e-5)
+    ### get poisson EM results
+    f_tau, tau = poiEM_results(dwell, n_components_p, tolerance=10e-5)
+    all_tau += [tau]
+    all_ftau += [f_tau]
 
-    ##  plot EM results(mean, std, fractio with iteration)
-    plot_EM_results_exp([f_save, tau_save], ['fraction', 'dwell time'])
 
-    ##  plot data histogram and its gaussian EM (GMM) results
-    plot_fit_pdf(dwell, exp_pdf, [f_save[-1,:], tau_save[-1,:]])
-
-    all_tau += [tau_save[-1,:]]
-    all_ftau += [f_save[-1,:]]
-
-    all_step += [step]
-    all_dwell += [dwell]
 
 
