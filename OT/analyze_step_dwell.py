e26028d3e6c5654f79c282a4ea5a6175b8ea8106
pine2104 <ychanc2104@gmail.com>
2021-03-23 11:38:59 +0800

    #7(OT) Add GPEM

--- OT/analyze_step_dwell.py
+++ OT/analyze_step_dwell.py
@@ -6,15 +6,15 @@ rcParams["font.sans-serif"] = ["Arial"]
 from basic.select import get_files, select_folder
 import scipy.io as sio
 import numpy as np
-from EM_Algorithm.EM import EM
-from lifelines import KaplanMeierFitter
-import pandas as pd
+from EM_Algorithm.EM import *
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
         
     conc = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0]
     # conc = [0.0, 1.0, 1.5, 2.0]
-    # conc = [3.0]
+    # conc = [1.0]
     
     path_folder = select_folder()
     steps = []
@@ -28,6 +28,11 @@ if __name__ == '__main__':
     all_tau = []
     all_ftau = []
     all_stau = []
+
+    all_2d_f = []
+    all_2d_m = []
+    all_2d_s = []
+    all_2d_tau = []
     for c in conc:
         path_data = get_files(f'S5S1_{c}*.mat', dialog=False, path_folder=path_folder)
         step = []
@@ -41,7 +46,7 @@ if __name__ == '__main__':
         EM_g = EM(step)
         n_components_g = EM_g.opt_components(tolerance=1e-2, mode='GMM', criteria='BIC', figure=False)
         f, m, s, labels, data_cluster = EM_g.GMM(n_components_g)
-        EM_g.plot_fit_gauss(scatter=True)
+        EM_g.plot_fit_gauss(scatter=True, xlim=[0,18])
 
         steps += [step]
         dwells += [dwell]
@@ -55,10 +60,28 @@ if __name__ == '__main__':
         EM_p = EM(dwell)
         n_components_p = EM_p.opt_components(tolerance=1e-2, mode='PEM', criteria='AIC', figure=False)
         f_tau, tau, s_tau, labels, data_cluster = EM_p.PEM(n_components_p)
-        EM_p.plot_fit_exp()
+        EM_p.plot_fit_exp(xlim=[0,10])
         all_ftau += [f_tau[-1, :]]
         all_tau += [tau[-1, :]]
         all_stau += [s_tau[-1,:]]
+
+        ##  2D clustering
+        step_dwell = np.array([step, dwell]).T
+        # plt.plot(data_g, data_p, 'o')
+        EM_gp = EM(step_dwell, dim=2)
+        # n_components_gp = EM_gp.opt_components(tolerance=1e-2, mode='GP', criteria='BIC', figure=False)
+
+
+        f1, m1, s1, f2, tau1 = EM_gp.GPEM(n_components=n_components_g, tolerance=1e-10, rand_init=True)
+        para = [f1[-1].ravel(), m1[-1].ravel(), s1[-1].ravel(), f2[-1].ravel(), tau1[-1].ravel()]
+        labels, data_cluster = EM_gp.predict(step_dwell, function=ln_gau_exp_pdf, paras=para)
+        plt.figure()
+        for x in data_cluster:
+            plt.plot(x[:, 0], x[:, 1], 'o')
+        all_2d_f += [f1[-1,:]]
+        all_2d_m += [m1[-1,:]]
+        all_2d_s += [s1[-1,:]]
+        all_2d_tau += [tau1[-1,:]]
 
 
     
