e66be80b8247f461f5d79c6990d4e1b700f96260
pine2104 <ychanc2104@gmail.com>
2021-05-09 22:25:26 +0800

    Add basic.noise for 2D noise

--- basic/noise.py
+++ basic/noise.py
@@ -4,3 +4,7 @@ import random
 def normal(n, m=0, s=1):
     noise = np.array([random.gauss(m, s) for i in range(n)])
     return noise
+
+def normal_2d(aoi_size, m=0, s=1):
+    noise = [random.gauss(m, s) for i in range(aoi_size**2)]
+    return np.array(noise).reshape(aoi_size, aoi_size)
