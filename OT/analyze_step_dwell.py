dd280510bc44ae9dadaa13a5a75d0e985b2573fd
pine_lab <ychanc2104@gmail.com>
2021-06-14 21:59:05 +0800

    #20(OT) add save to .xlsx

--- OT/analyze_step_dwell.py
+++ OT/analyze_step_dwell.py
@@ -4,11 +4,15 @@ rcParams["font.family"] = "sans-serif"
 rcParams["font.sans-serif"] = ["Arial"]
 
 from basic.select import get_files, select_folder
+from FRET.cluster_FRET import get_col, gen_random_code
 import scipy.io as sio
-import numpy as np
+import pandas as pd
 from EM_Algorithm.EM import *
 import matplotlib.pyplot as plt
 
+def remove_steps(step, dwell, criteria):
+    booleans = step < criteria
+    return step[booleans], dwell[booleans]
 
 if __name__ == '__main__':
     all_gauss = []
@@ -16,13 +20,15 @@ if __name__ == '__main__':
     all_results = []
     
     n_sample = []
-    # conc = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0]  ## S5S1
+    conc = [0.0, 0.5, 1.0, 1.5, 2.0, 3.0]  ## S5S1
     # conc = [1.0, 1.2, 1.5, 1.8, 2.0, 3.0, 4.0] ## m51 only
-    conc = ['0.10', '0.20', '0.25', '0.50', '0.70', '0.80', '1.10', '1.20', '2.00']  ## EcRecA
-    conc = ['2.0']
+    # conc = ['0.10', '0.20', '0.25', '0.50', '0.70', '0.80', '1.10', '1.20', '2.00']  ## EcRecA
+    conc = [3.0]
     # path_folder = select_folder()
     # path_folder = r'/home/hwligroup/Desktop/Data/step-dwell time/m51'
-    path_folder = r'C:\Users\pine\Desktop\Data\step-dwell time\m51 + mSS'
+    path_folder = r'/home/hwligroup/Desktop/Data/step-dwell time/m51 + mSS'
+
+    # path_folder = r'C:\Users\pine\Desktop\Data\step-dwell time\m51 + mSS'
 
     for i in range(1):
 
@@ -36,6 +42,7 @@ if __name__ == '__main__':
                 data = sio.loadmat(path)
                 step = np.append(step, data['step'])
                 dwell = np.append(dwell, [data['dwell']])
+                step, dwell = remove_steps(step, dwell, criteria=15)
             n_sample += [len(step)]
     
             ## get Gaussian EM results
@@ -57,15 +64,28 @@ if __name__ == '__main__':
             f1, m1, s1, tau1, converged_gp, LLE_gp = EM_gp.GPEM(n_components=opt_components, tolerance=1e-2, rand_init=True)
 
             ##  plot figure
-            EM_g.plot_fit_gauss(scatter=False, xlim=[0, 20], save=False, path=f'{c}_gauss.png', figsize=(7,2))
-            EM_p.plot_fit_exp(xlim=[0, 10], save=False, path=f'{c}_survival.png', figsize=(7,2))
+            # EM_g.plot_fit_gauss(scatter=False, xlim=[0, 16], save=False, path=f'{c}_gauss.png', figsize=(7,2))
+            # EM_p.plot_fit_exp(xlim=[0, 10], save=False, path=f'{c}_survival.png', figsize=(7,2))
             para = EM_gp.para_final
             EM_p.plot_fit_exp(xlim=[0, 10], save=False, path=f'{c}_survival.png', figsize=(7,2),
                               para=[para[0]]+[para[3]])
 
             # EM_gp.plot_gp_contour(xlim=[0, 20], ylim=[0, 10.], save=True, path=f'{c}_2D.png')
             # EM_gp.plot_gp_surface()
-            EM_gp.plot_gp_contour_2hist(xlim=[0,20], ylim=[0,10])
+            EM_gp.plot_gp_contour_2hist(xlim=[0,16], ylim=[0,10], bins_x=np.log2(len(step)).astype('int'))
             
             all_results += [np.array([f1, m1, s1, tau1, converged_gp, LLE_gp]).T]
+            print(all_results)
+
+        # df_f = pd.DataFrame(f1, columns=['f'])
+        # df_m = pd.DataFrame(m1, columns=['center'])
+        # df_s = pd.DataFrame(s1, columns=['std'])
+        # df_tau = pd.DataFrame(tau1, columns=['tau'])
+        # df_LLE = pd.DataFrame(LLE_gp, columns=['LLE'])
+        # df = pd.concat([df_f, df_m, df_s, df_tau, df_LLE], axis=1)
+        #
+        # ## save to disk
+        # writer = pd.ExcelWriter( f'{gen_random_code(3)}_EM_results.xlsx')
+        # df.to_excel(writer, sheet_name='EM', index=True)
+        # writer.save()
 
